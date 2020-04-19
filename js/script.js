d0009b1fc8a2ac2b26debc469c4a9b5f5a41dc02
cchau2 <59983801+cchau2@users.noreply.github.com>
2020-04-18 20:07:04 -0500

    js file update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,10 @@
 function init(){
-//add your javascrip between these two lines of code
- 
-
-
-
-
-
-
-
+	var button = document.getElementById('entrybutton');
+	var input = document.getElementById('entryinput');
+	var output = document.getElementById('textoutput');
+	button.addEventListener('click', function(){
+		output.innerHTML = input.value; 
+		alert("Cynthia Chau: " + input.value);
+		} );
+}
 window.addEventListener('load', init);
